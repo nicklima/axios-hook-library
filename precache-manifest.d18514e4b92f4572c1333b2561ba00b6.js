75c0913521af75b0fb5921ef6c9f7896ce6bc7a7
Nick Lima <nick.lima.wp@gmail.com>
2021-04-10 08:57:07 -0300

    Updates

---
+++ precache-manifest.d18514e4b92f4572c1333b2561ba00b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6872e28c076a936ad11db193b64acdc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9251e1f6c5f28543ab4e",
+    "url": "./static/css/main.e7f97c56.chunk.css"
+  },
+  {
+    "revision": "eedc40f7e7e43263c9f0",
+    "url": "./static/js/2.664e4b61.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.664e4b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9251e1f6c5f28543ab4e",
+    "url": "./static/js/main.16ace1b9.chunk.js"
+  },
+  {
+    "revision": "7db2e3bb8e5370e7a981",
+    "url": "./static/js/runtime-main.acd00787.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "./static/media/logo.06e73328.svg"
+  }
+]);
